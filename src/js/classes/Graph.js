b7097bfbfceab631035f29ada3b739040cf7721b
Alex Graves <alexwoodgraves@gmail.com>
2021-01-01 20:45:46 -0500

    add scatter plot title and subhead styling

---
+++ src/js/classes/Graph.js
@@ -0,0 +1,8 @@
+export class ScatterPlot {
+  constructor(data, xAxis, yAxis, container) {
+    this.data = data;
+    this.xAxis = xAxis;
+    this.yAxis = yAxis;
+    this.container = container;
+  }
+}
---
+++ src/js/scatter-plot.js
@@ -0,0 +1,25 @@
+import { ScatterPlot } from "./classes/Graph.js";
+import { RACE_SCATTER_PLOT } from "./data.js";
+
+/* PLOT CREATION FUNCTIONS */
+const createRaceScatterPlot = () => {
+  const xAxis = {
+    name: "Cash Bail Rate",
+    min: 0,
+    max: 100
+  };
+  const yAxis = {
+    name: "Bail Amount",
+    min: 0,
+    max: 80000
+  };
+
+  const container = document.getElementById("race-scatter-plot");
+  return new ScatterPlot(RACE_SCATTER_PLOT, xAxis, yAxis, container);
+};
+
+// for converting dollar strings to numbers:
+// Number(value.replace(/[^\d.-]/g, ""))
+
+/* RENDER PAGE */
+createRaceScatterPlot();
