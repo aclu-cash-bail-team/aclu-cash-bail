36b42448c7b8a8d593d385fedfbb97a00ee1af79
Alex Graves <alexwoodgraves@gmail.com>
2021-01-02 19:37:43 -0500

    use svg to make plotting lines easier

--- src/js/classes/Graph.js
+++ src/js/classes/Graph.js
@@ -1,4 +1,5 @@
 const POINT_RADIUS = 4;
+const SVG_NS = "http://www.w3.org/2000/svg";
 
 class CountyPoint {
   constructor(county, data, xAxis, yAxis, outlier, plot) {
@@ -8,22 +9,46 @@ class CountyPoint {
     this.yAxis = yAxis;
     this.outlier = outlier;
     this.plot = plot;
+    [this.xs, this.ys] = this.getPositions();
   }
 
   isOutlier() {
     return this.outlier;
   }
 
-  render() {
+  getPositions() {
+    const xs = [], ys = [];
     for (const data of this.data) {
-      const point = document.createElement("div");
-      let className = `${data.class} scatter-point`;
-      if (this.outlier) className += " outlier";
-      point.className = className;
-      point.style.left = `calc(${data.x / this.xAxis.max * 100}% - ${POINT_RADIUS}px`;
-      point.style.bottom = `calc(${data.y / this.yAxis.max * 100}% - ${POINT_RADIUS}px`;
-      this.plot.appendChild(point);
+      xs.push(`${data.x / this.xAxis.max * 100}%`);
+      // svgs start Y from the top, so subtract the percentage from 100
+      ys.push(`${100 - (data.y / this.yAxis.max * 100)}%`);
     }
+    return [xs, ys];
+  }
+
+  renderPoints() {
+    this.data.forEach((data, i) => {
+      let className = `${data.class} scatter${this.outlier ? " outlier" : ""}`;
+      const point = document.createElementNS(SVG_NS, "circle");
+      point.setAttributeNS(null, "class", className);
+      point.setAttributeNS(null, "cx", this.xs[i]);
+      point.setAttributeNS(null, "cy", this.ys[i]);
+      point.setAttributeNS(null, "r", POINT_RADIUS);
+      this.plot.appendChild(point)
+    });
+  }
+
+  renderLine() {
+    if (this.data.length != 2) return;
+
+    const line = document.createElementNS(SVG_NS, "line");
+    let className = `scatter${this.outlier ? " outlier" : ""}`;
+    line.setAttributeNS(null, "class", className);
+    line.setAttributeNS(null, "x1", this.xs[0]);
+    line.setAttributeNS(null, "y1", this.ys[0]);
+    line.setAttributeNS(null, "x2", this.xs[1]);
+    line.setAttributeNS(null, "y2", this.ys[1]);
+    this.plot.appendChild(line);
   }
 }
 
@@ -79,6 +104,8 @@ export class ScatterPlot {
   }
 
   render() {
-    this.points.forEach(point => point.render());
+    // render all lines first so that points are on top
+    this.points.forEach(point => point.renderLine());
+    this.points.forEach(point => point.renderPoints());
   }
 }
