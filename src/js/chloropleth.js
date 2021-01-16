c10097862421a51dce28cbcc6b5959fa3b45190e
Arjun Pothukuchi <arjun19403@gmail.com>
2021-01-16 08:29:55 -1000

    Add initial bail race map implementation

--- src/js/chloropleth.js
+++ src/js/chloropleth.js
@@ -1,5 +1,10 @@
-import { BailRateMap } from "./classes/Map.js";
-import { BAIL_RATE_DATA, ROR_RATE_DATA } from "./data.js";
+import { RaceMapContainer, BailRaceMap, BailRateMap } from "./classes/Map.js";
+import { BAIL_RATE_DATA, ROR_RATE_DATA, BAIL_RACE_RATE_DATA } from "./data.js";
 
 new BailRateMap(BAIL_RATE_DATA, 42.6);
 new BailRateMap(ROR_RATE_DATA, 22.7);
+const container = new RaceMapContainer();
+const black = new BailRaceMap(BAIL_RACE_RATE_DATA, 2, "black", container);
+const white = new BailRaceMap(BAIL_RACE_RATE_DATA, 3, "white", container);
+container.registerBlack(black);
+container.registerWhite(white);
